bb921425d96bd78f158d78b19b80e4512d71be05
Chris <2648373+QuiCM@users.noreply.github.com>
2018-10-26 20:16:49 +1030

    Commenting and implementation of non-blocking connection

--- Gateway/Connector.cs
+++ Gateway/Connector.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Descriptors;
 using Discord.Descriptors.Guilds;
 using Discord.Http.Gateway;
+using Discord.Json.Objects;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -27,9 +28,17 @@ namespace Discord.Gateway
         /// </summary>
         protected WebSocket Socket { get; }
 
+        /// <summary>
+        /// TokenSource created by linking the Gateway's CancellationToken and an end-user provided token
+        /// </summary>
+        internal CancellationTokenSource linkedTokenSource;
+
         public List<GuildDescriptor> Guilds { get; protected set; }
         public List<UserDescriptor> Users { get; protected set; }
 
+        /// <summary>
+        /// Token provided by an end-user for them to manage cancellation with
+        /// </summary>
         private CancellationToken _externalToken;
 
         /// <summary>
@@ -50,41 +59,37 @@ namespace Discord.Gateway
         /// </summary>
         /// <param name="externalToken">CancellationToken used for managing cancellation of the connection</param>
         /// <returns></returns>
-        public virtual async Task ConnectAsync(CancellationToken externalToken)
+        public virtual async Task<Task> ConnectAsync(CancellationToken externalToken)
         {
+            if (externalToken == null) 
+            {
+                throw new ArgumentNullException("externalToken");
+            }
+
             _externalToken = externalToken;
+            //Create a TokenSource from the provided client CancellationToken and our own internal token source's token
+            //If the client ever cancels their token then this TokenSource will propagate cancellation
+            //Likewise, if we ever cancel our token then this TokenSource will propagate cancellation
+            linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(externalToken, Gateway.GatewayTokenSource.Token);
 
-            try
-            {
-                using (CancellationTokenSource linkedCts = CancellationTokenSource.CreateLinkedTokenSource(
-                    externalToken,
-                    Gateway.GatewayTokenSource.Token))
-                {
-                    if (!await AuthenticateAsync(linkedCts.Token))
-                    {
-                        Gateway.GatewayTokenSource.Cancel();
-                        linkedCts.Token.ThrowIfCancellationRequested();
-                    }
+            //Retrieve information required to connect to the Gateway.
+            //This will throw an exception if it fails.
+            //Should we handle the exception and cancel, or let it bubble?
+            GetGatewayBotResponseObject connectionProperties = await AuthenticateAsync(linkedTokenSource.Token);
 
-                    Task.WaitAny(new[] {
-                            HeartbeatAsync(linkedCts.Token),
-                            //Connect, ready, message pump, etc
-                        },
-                        linkedCts.Token
-                    );
-                }
-            }
-            catch (OperationCanceledException)
-            {
-                if (externalToken.IsCancellationRequested)
-                {
-                    externalToken.ThrowIfCancellationRequested();
-                }
-                else
-                {
-                    //internal cancellation
-                }
-            }
+            //Use the retrieved information to create a new WebSocket instance, then connect to it
+            Socket = new WebSocket(connectionProperties.url, GatewayRoutes.Encoding);
+            //We send the linked TokenSource token through so that cancellation will propagate to/from the WebSocket.
+            //The returned task can be awaited to block
+            Task blockable = await Socket.ConnectAsync(linkedTokenSource.Token);
+            //When blockable completes we can assume that we have disconnected from the gateway 
+            //and so should cancel the Gateway's token
+            blockable = blockable.ContinueWith(task => Gateway.GatewayTokenSource.Cancel()); 
+
+            //heartbeat logic
+
+            //Return the blockable task so that clients can block
+            return blockable;
         }
 
         /// <summary>
@@ -104,7 +109,12 @@ namespace Discord.Gateway
 
             if (!Gateway.GatewayTokenSource.IsCancellationRequested)
             {
+                //Cancelling our token should cause the WebSocket and everything else to disconnect
                 Gateway.GatewayTokenSource.Cancel();
+            }
+            else 
+            {
+                throw new InvalidOperationException("Disconnection already pending.");
             }
         }
 
@@ -113,14 +123,16 @@ namespace Discord.Gateway
         /// </summary>
         /// <param name="token">CancellationToken used to handle cancellation of the connection</param>
         /// <returns>awaitable boolean. True if auth succeeded, else false</returns>
-        public virtual async Task<bool> AuthenticateAsync(CancellationToken token)
+        public virtual async Task<GetGatewayBotResponseObject> AuthenticateAsync(CancellationToken token)
         {
+            //Doesn't this require authentication?
+
             token.ThrowIfCancellationRequested();
 
-            Json.Objects.GetGatewayBotResponseObject response =
-                (Json.Objects.GetGatewayBotResponseObject)await GatewayRoutes.GetGatewayAsync(Gateway.Rest, token);
+            GetGatewayBotResponseObject response =
+                (GetGatewayBotResponseObject)await GatewayRoutes.GetGatewayAsync(Gateway.Rest, token);
 
-            return true;
+            return response;
         }
 
         /// <summary>
